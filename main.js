308114208a225cbe746b2bd57433211571d5694b
Carmen O'Grady <22309co@hvhs.school.nz>
2025-03-17 21:46:21 +0000

    bonk

--- main.js
+++ main.js
@@ -1,28 +1,30 @@
-// Global variables
-let gameState = "start";  // Initial game state is 'start'
-let meteor;              
-let meteorImg;          
+/*******************************************************/
+// Global Variables
+/*******************************************************/
+let gameState = "start"; 
+let remmy;  
 
+/*******************************************************/
+// setup()
+/*******************************************************/
 function setup() {
-  // Create the canvas and set gravity for physics engine
-  createCanvas(windowWidth, windowHeight);
+	console.log("setup: ");
+	createCanvas(windowWidth, windowHeight);
 
-  
-  // Create the start button and set up the event listener
   let startButton = select('#start-button');
-  startButton.mousePressed(startGame);  // When button is pressed, start the game
-  
-  // Load the meteor image from the file
-  meteorImg = loadImage('pngimg.com - meteor_PNG22.png');  
+  if (startButton) {
+    startButton.mousePressed(startGame);
 
-  // Create the meteor sprite at the center of the canvas
-  meteor = createSprite(width / 2, height / 2, 50, 50);  
-  meteor.addImage(meteorImg); 
-  meteor.scale = 0.2;  
+  remmy = new Sprite(200, 150, 100, 100,'d');
+  remmy.shapeColor = 'yellow';
+  remmy.rotationSpeed = 18;
+	remmy.vel.x = -2;
+  background('black');
 }
-
+/*******************************************************/
+// draw()
+/*******************************************************/
 function draw() {
-  background(0);  
 
   // Check the current game state
   if (gameState === "start") {
@@ -31,35 +33,22 @@ function draw() {
     playGame();  
   }
 
-  // Draw all sprites on the canvas
-  drawSprites();  
+  drawSprites(); 
 }
-
-function displayStartScreen() {
-  // Display the game title on the start screen
-  textAlign(CENTER, CENTER); 
-  fill(255);
-  textSize(32);
-  text("Meteor Rush", width / 2, height / 3);  // Display title in the center of the screen
-}
-
+/*******************************************************/
+// startGame()
+/*******************************************************/
 function startGame() {
   console.log("Start Game button clicked!");
-
   gameState = "playing";  // Change game state to 'playing'
   
-  // Hide the start button and title once the game starts
   let startButton = select('#start-button');
-  startButton.hide();  
-  
+  if (startButton) {
+    startButton.hide();  
+  }
+
   let title = select('.title');  
-  title.hide();  
+  if (title) {
+    title.hide();
+  }
 }
-
-function playGame() {
-  // Make the meteor move randomly within a certain range
-  meteor.position.x += random(-5, 5); 
-  meteor.position.y += random(-5, 5); 
-}
-
-
